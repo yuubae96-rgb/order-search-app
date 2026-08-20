1d76cff4c39b0c7eb172717561e92b5338f484c5
yuubae96-rgb <yuubae96@gmail.com>
2026-08-21 08:00:59 +0900

    Load safe material delete controls

--- supabase.js
+++ supabase.js
@@ -63,3 +63,4 @@ loadCompanyModule('production-summary.js?v=20260819-0110');
 loadCompanyModule('materials-patch.js?v=20260819-0135');
 loadCompanyModule('material-auth-stop.js?v=20260821-0405');
 loadCompanyModule('material-unit-fix.js?v=20260821-0753');
+loadCompanyModule('material-delete.js?v=20260821-0802');
